640017707fa6d059d6026b15f9d23ecf55019349
Creastacean <kes8794bg@gmail.com>
2020-03-09 13:28:11 -0600

    Added test comment

--- app/src/main/java/com/example/artapp/MainActivity.java
+++ app/src/main/java/com/example/artapp/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //I like this comment better, so i deleted the first one :)
         //here' another comment
+        //you guys are ridiculous
     }
 }

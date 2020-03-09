f0a4a03750459f0318d366c2bfdf926c7c142260
har15053 <har15053@byui.edu>
2020-03-09 13:24:46 -0600

    Changed comment

--- app/src/main/java/com/example/artapp/MainActivity.java
+++ app/src/main/java/com/example/artapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is to test Android GitHub connection
+        //I like this comment better, so i deleted the first one :)
     }
 }

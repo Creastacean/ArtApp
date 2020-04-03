064445d30ec90b961755bfcd2b357bdd5f064a8c
Creastacean <kes8794bg@gmail.com>
2020-04-03 14:13:57 -0600

    starting to add value event listener, hop it works

--- app/src/main/java/com/example/artapp/MainActivity.java
+++ app/src/main/java/com/example/artapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.artapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     //I don't think we need this here, need to check
     PostMaker postMaker;
+    DatabaseReference database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +31,26 @@ public class MainActivity extends AppCompatActivity {
         //you guys are ridiculous
         //Don't make fun of me! YOu're not my mom!!
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(PostMaker.EXTRA_THING);
+        final String message = "no posts";
+        database = FirebaseDatabase.getInstance().getReference().child("Post");
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        database.addValueEventListener(ValueEventListener listener);
+
+
+
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(PostMaker.EXTRA_THING);
         //I got a string back to the main activity. but i am not sure how to handle the obvious
         //case where we haven't even left the main activity yet, let alone returned.
         //how do we determin that is the case, once that is understood then it should be a simple
